7d80bc06422d4fc64a2ce6eafdc05d6827570dad
app_liziping <822548292@qq.com>
2016-11-11 18:21:16 +0800

    修改

---
+++ app/src/main/java/com/risenb/ykj/home/activity/Mina2Activity.java
@@ -0,0 +1,19 @@
+package com.risenb.ykj.home.activity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+import com.risenb.ykj.R;
+import com.risenb.ykj.base.BaseAppCompatActivity;
+
+/**
+ * Created by Administrator on 2016/11/10.
+ */
+
+public class Mina2Activity extends BaseAppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_main2);
+    }
+}
